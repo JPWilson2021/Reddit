a0a3ff4f30162564b261be92cac765ae0ab626d0
JPWilson2021 <wilsonjp805@gmail.com>
2022-08-28 01:07:37 -0400

    Created a separate header component to clean up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,19 @@
 import './style.css';
 import Logo from './clipart-reddit.png';
 import Avatar from './Snoo.png';
+import Banner from './RedditBanner.png';
 import {MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import {BellIcon, PlusIcon} from "@heroicons/react/24/outline";
 import {ChatBubbleOvalLeftEllipsisIcon, ChevronDownIcon} from "@heroicons/react/20/solid";
+import Header from "./Header";
 
 function App() {
   return (
     <div>
-        <header className="w-full bg-reddit_dark p-2">
-            <div className="flex mx-4">
-                <img src={Logo} alt="" className="w-8 h-8 mr-4"/>
-                <form action="" className="bg-reddit_dark-brighter px-3 flex rounded-md border border-gray-700 mx-4 flex-grow">
-                    <MagnifyingGlassIcon className="text-gray-300 h-6 w-6 mt-1" />
-                    <input type="text" className="bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Search"/>
-                </form>
-                <button className="px-2 py-1">
-                    <ChatBubbleOvalLeftEllipsisIcon className="text-white w-6 h-6 m-1 mx-2"/>
-                </button>
-                <button className="px-2 py-1">
-                    <BellIcon className="text-white w-6 h-6 m-1 mx-2"/>
-                </button>
-                <button className="px-2 py-1">
-                    <PlusIcon className="text-white w-6 h-6 m-1 mx-2"/>
-                </button>
-                <button className="rounded-md h-8 flex ml-4">
-                    <img src={Avatar} alt="" className="block w-8 h-8"/>
-                    <ChevronDownIcon className="text-white w-5 h-5 mt-1"/>
-                </button>
-            </div>
-        </header>
+        <Header/>
+        <section className="subheader">
+            <img src={Banner} alt="" />
+        </section>
     </div>
   );
 }
---
+++ client/src/Header.js
@@ -0,0 +1,34 @@
+import Logo from "./clipart-reddit.png";
+import {MagnifyingGlassIcon} from "@heroicons/react/24/solid";
+import {ChatBubbleOvalLeftEllipsisIcon, ChevronDownIcon} from "@heroicons/react/20/solid";
+import {BellIcon, PlusIcon} from "@heroicons/react/24/outline";
+import Avatar from "./Snoo.png";
+
+function Header(){
+    return (
+        <header className="w-full bg-reddit_dark p-2">
+            <div className="flex mx-4">
+                <img src={Logo} alt="" className="w-8 h-8 mr-4"/>
+                <form action="" className="bg-reddit_dark-brighter px-3 flex rounded-md border border-gray-700 mx-4 flex-grow">
+                    <MagnifyingGlassIcon className="text-gray-300 h-6 w-6 mt-1" />
+                    <input type="text" className="bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Search"/>
+                </form>
+                <button className="px-2 py-1">
+                    <ChatBubbleOvalLeftEllipsisIcon className="text-white w-6 h-6 m-1 mx-2"/>
+                </button>
+                <button className="px-2 py-1">
+                    <BellIcon className="text-white w-6 h-6 m-1 mx-2"/>
+                </button>
+                <button className="px-2 py-1">
+                    <PlusIcon className="text-white w-6 h-6 m-1 mx-2"/>
+                </button>
+                <button className="rounded-md h-8 flex ml-4">
+                    <img src={Avatar} alt="" className="block w-8 h-8"/>
+                    <ChevronDownIcon className="text-white w-5 h-5 mt-1"/>
+                </button>
+            </div>
+        </header>
+
+    );
+}
+export default Header;
