7322b0f7256bcb270a33e10e5940fe5671e0506c
JPWilson2021 <wilsonjp805@gmail.com>
2022-09-04 23:26:24 -0400

    Added Button component

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,6 +3,7 @@ import {MagnifyingGlassIcon} from "@heroicons/react/24/solid";
 import {ChatBubbleOvalLeftEllipsisIcon, ChevronDownIcon} from "@heroicons/react/20/solid";
 import {BellIcon, PlusIcon} from "@heroicons/react/24/outline";
 import Avatar from "./Snoo.png";
+import Button from "./Button";
 
 function Header(){
     return (
@@ -13,15 +14,22 @@ function Header(){
                     <MagnifyingGlassIcon className="text-gray-300 h-6 w-6 mt-1" />
                     <input type="text" className="bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Search"/>
                 </form>
-                <button className="px-2 py-1">
-                    <ChatBubbleOvalLeftEllipsisIcon className="text-white w-6 h-6 m-1 mx-2"/>
-                </button>
-                <button className="px-2 py-1">
-                    <BellIcon className="text-white w-6 h-6 m-1 mx-2"/>
-                </button>
-                <button className="px-2 py-1">
-                    <PlusIcon className="text-white w-6 h-6 m-1 mx-2"/>
-                </button>
+
+                {/*<button className="px-2 py-1">*/}
+                {/*    <ChatBubbleOvalLeftEllipsisIcon className="text-white w-6 h-6 m-1 mx-2"/>*/}
+                {/*</button>*/}
+                {/*<button className="px-2 py-1">*/}
+                {/*    <BellIcon className="text-white w-6 h-6 m-1 mx-2"/>*/}
+                {/*</button>*/}
+                {/*<button className="px-2 py-1">*/}
+                {/*    <PlusIcon className="text-white w-6 h-6 m-1 mx-2"/>*/}
+                {/*</button>*/}
+               <Button>Log In</Button>
+               <Button>Sign Up</Button>
+
+
+
+
                 <button className="rounded-md h-8 flex ml-4">
                     <img src={Avatar} alt="" className="block w-8 h-8"/>
                     <ChevronDownIcon className="text-white w-5 h-5 mt-1"/>
